6c3182896fc15986a9cd8af08b55faab8abd46ef
Alf-Rune Siqveland <alf.rune@northern.tech>
2024-06-12 12:56:55 +0200

    chore: Enable replication oplog for mongodb in dev setup
    Useful for debugging database writes and enables transactions and change
    streams :sparkles:
    
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

---
+++ dev/config/mongo.init.js
@@ -0,0 +1,13 @@
+try {
+	rs.status()
+} catch (exception) {
+	if (exception.codeName === "NotYetInitialized") {
+		rs.initiate({
+			_id: "rs0",
+			members: [{
+				_id: 0,
+				host: "127.0.0.1",
+			}],
+		});
+	}
+}
