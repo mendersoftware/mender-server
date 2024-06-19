988d9edffc225603da5b4724fb803e568f315c2e
Fabio Tranchitella <fabio.tranchitella@northern.tech>
2024-06-19 17:50:24 +0200

    fix(tests): do not fail if env variables are not set, use hard-coded defaults
    Changelog: None
    Ticket: QA-686
    
    Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>

--- backend/tests/integration/tests/test_aws_iotcore.py
+++ backend/tests/integration/tests/test_aws_iotcore.py
@@ -167,6 +167,10 @@ class _TestAWSIoTCoreBase:
         )
 
 
+@pytest.mark.skipif(
+    not bool(os.environ.get("AWS_IOTCORE_ACCESS_KEY_ID")),
+    reason="AWS_IOTCORE_ACCESS_KEY_ID not provided",
+)
 class TestAWSIoTCoreIntegrations(_TestAWSIoTCoreBase):
     @pytest.mark.skipif(
         not bool(os.environ.get("AWS_IOTCORE_ACCESS_KEY_ID")),
@@ -198,6 +202,10 @@ class TestAWSIoTCoreIntegrations(_TestAWSIoTCoreBase):
         self.check_integrations(user, expected_integration)
 
 
+@pytest.mark.skipif(
+    not bool(os.environ.get("AWS_IOTCORE_ACCESS_KEY_ID")),
+    reason="AWS_IOTCORE_ACCESS_KEY_ID not provided",
+)
 class TestAWSIoTCoreIntegrationsEnterprise(_TestAWSIoTCoreBase):
     @pytest.mark.skipif(
         not bool(os.environ.get("AWS_IOTCORE_ACCESS_KEY_ID")),
