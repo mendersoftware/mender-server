7a6882bd9f5c337c274d27a358a424afb4a0a556
Manuel Zedel <manuel.zedel@northern.tech>
2025-02-18 22:38:32 +0100

    chore: debug
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- frontend/tests/e2e_tests/integration/10-webhooks.spec.ts
+++ frontend/tests/e2e_tests/integration/10-webhooks.spec.ts
@@ -59,7 +59,6 @@ test.describe('Webhooks Functionality', () => {
   });
   test('allows configuring inventory webhooks', async ({ baseUrl, environment, loggedInPage: page }) => {
     test.skip(!isEnterpriseOrStaging(environment));
-    await stopDockerClient();
     await page.goto(`${baseUrl}ui/settings/integrations`);
     await page.getByLabel(/add an integration/i).click();
     await page.getByRole('option', { name: /Webhooks/i }).click();
@@ -67,13 +66,14 @@ test.describe('Webhooks Functionality', () => {
     await page.getByLabel(/url/i).fill(`${baseWebhookLocation}/inventory`);
     await page.getByLabel(/device authentication/i).click();
     await page.getByLabel(/device inventory/i).click();
-    const token = await tenantTokenRetrieval(baseUrl, page);
-    await startDockerClient(baseUrl, token);
     await page.getByRole('button', { name: /save/i }).click();
     await expect(page.getByText(/view details/i)).toBeVisible();
     await expect(page.getByText(/one active integration at a time/i)).toBeVisible();
   });
   test('shows webhook details for inventory events', async ({ baseUrl, loggedInPage: page }) => {
+    await stopDockerClient();
+    const token = await tenantTokenRetrieval(baseUrl, page);
+    await startDockerClient(baseUrl, token);
     await page.goto(`${baseUrl}ui/settings/integrations`);
     await page.getByText(/view details/i).click();
     await page
