4729e13c451a20ace4c57ef0f564a38e56107465
Manuel Zedel <manuel.zedel@northern.tech>
2025-04-10 14:28:02 +0200

    test(gui): extracted e2e login step to dependency stage in order to reuse login state
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- frontend/tests/e2e_tests/integration/00-setup.spec.ts
+++ frontend/tests/e2e_tests/integration/00-setup.spec.ts
@@ -12,11 +12,8 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 import { expect } from '@playwright/test';
-import * as fs from 'fs';
 
 import test from '../fixtures/fixtures.ts';
-import { isEnterpriseOrStaging, isLoggedIn, login, prepareNewPage, startDockerClient, stopDockerClient, tenantTokenRetrieval } from '../utils/commands.ts';
-import { selectors, storagePath, timeouts } from '../utils/constants.ts';
 
 test.describe('Test setup', () => {
   test.describe('basic window checks', () => {
@@ -25,12 +22,6 @@ test.describe('Test setup', () => {
       await page.goto(`${baseUrl}ui/`);
       const theWindow = await page.evaluate(() => window.innerWidth);
       expect(theWindow).toBeDefined();
-      try {
-        fs.unlinkSync('loginInfo.json');
-        await stopDockerClient();
-      } catch {
-        // ...continue
-      }
     });
     test('get the document object', async ({ page }) => {
       const documentCharset = await page.evaluate(() => document.charset);
@@ -41,68 +32,6 @@ test.describe('Test setup', () => {
       page = await context.newPage();
       await page.goto(`${baseUrl}ui/`);
       await expect(page).toHaveTitle(/Mender/i);
-    });
-  });
-
-  test.describe('account creation', () => {
-    test('allows account creation', async ({ baseUrl, context, environment, page, password, request, username }) => {
-      test.skip(environment !== 'staging');
-      try {
-        const { token } = await login(username, password, baseUrl, request);
-        test.skip(!!token, 'looks like the account was created already, continue with the remaining tests');
-      } catch {
-        // looks like this is the first run, let's continue
-      }
-      await page.goto(`${baseUrl}ui/`);
-      await expect(page.getByText(/Sign up/i)).toBeVisible();
-      await page.getByText(/Sign up/i).click();
-      console.log(`creating user with username: ${username} and password: ${password}`);
-      await expect(page.getByText(/Sign up with/i)).toBeVisible();
-      await page.fill(selectors.email, username);
-      await page.fill(selectors.password, password);
-      await page.fill(selectors.password, '');
-      await page.fill(selectors.password, password);
-      await page.fill(selectors.passwordConfirmation, password);
-
-      await page.getByRole('button', { name: /Sign up/i }).click();
-      await page.getByRole('button', { name: /Complete/i }).waitFor();
-      await page.getByLabel(/organization name/i).fill('CI test corp');
-      await page.getByLabel(/terms of service/i).check();
-      const frameHandle = await page.waitForSelector('iframe[title="reCAPTCHA"]');
-      await page.waitForTimeout(300);
-      const recaptchaFrame = await frameHandle.contentFrame();
-      await recaptchaFrame.waitForSelector('#recaptcha-anchor');
-      const recaptcha = await recaptchaFrame.$('#recaptcha-anchor');
-      await recaptcha.click();
-      await page.waitForTimeout(timeouts.default);
-      await page.getByRole('button', { name: /Complete/i }).click();
-      await isLoggedIn(page, timeouts.fifteenSeconds);
-
-      // the following sets the UI up for easier navigation by disabling onboarding
-      const newPage = await prepareNewPage({ baseUrl, context, password, request, username });
-      await isLoggedIn(newPage);
-      await context.storageState({ path: storagePath });
-    });
-  });
-
-  test.describe('enterprise setting features, that happens to start up a docker client', () => {
-    test('supports tenant token retrieval', async ({ baseUrl, context, environment, password, request, username }) => {
-      test.skip(!isEnterpriseOrStaging(environment));
-      console.log(`logging in user with username: ${username} and password: ${password}`);
-      const page = await prepareNewPage({ baseUrl, context, password, request, username });
-      await page.goto(`${baseUrl}ui/settings`);
-      const isVisible = await page.getByRole('button', { name: /change email/i }).isVisible();
-      if (!isVisible) {
-        console.log('settings may not be loaded - move around');
-        await page.goto(`${baseUrl}ui/help`);
-        await page.goto(`${baseUrl}ui/settings`);
-      }
-      const token = await tenantTokenRetrieval(baseUrl, page);
-      if (environment === 'staging') {
-        await startDockerClient(baseUrl, token);
-      }
-      await context.storageState({ path: storagePath });
-      expect(token).toBeTruthy();
     });
   });
 });
