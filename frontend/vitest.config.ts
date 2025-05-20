7e0ecfaa22df926e5ea12f4f3f43bc7b67b1afdb
Manuel Zedel <manuel.zedel@northern.tech>
2025-05-20 13:32:21 +0200

    chore: debug pool parallelism 5
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,70 +1,57 @@
 import react from '@vitejs/plugin-react';
-import { cpus } from 'os';
 import path from 'path';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { UserWorkspaceConfig, defineConfig } from 'vitest/config';
 
-export default defineConfig(() => {
-  const isCi = process.env.CI;
-  const cpuCount = cpus().length;
-  const threadCount = isCi ? Math.max(1, cpuCount - 1) : undefined;
-
-  return {
-    plugins: [
-      react(),
-      svgr({
-        svgrOptions: {
-          ref: true,
-          svgo: false,
-          titleProp: true
+export default defineConfig(
+  () =>
+    ({
+      plugins: [
+        react(),
+        svgr({
+          svgrOptions: {
+            ref: true,
+            svgo: false,
+            titleProp: true
+          },
+          include: '**/*.svg'
+        }),
+        tsconfigPaths({ root: path.resolve(__dirname) })
+      ],
+      resolve: {
+        alias: [
+          {
+            find: '@northern.tech/store',
+            replacement: path.resolve(__dirname, 'src/js/store')
+          },
+          {
+            find: '@northern.tech/common-ui',
+            replacement: path.resolve(__dirname, 'src/js/common-ui')
+          }
+        ]
+      },
+      server: {
+        port: 80,
+        middlewareMode: false
+      },
+      test: {
+        coverage: {
+          reporter: ['json', 'lcov'],
+          reportsDirectory: 'coverage'
         },
-        include: '**/*.svg'
-      }),
-      tsconfigPaths({ root: path.resolve(__dirname) })
-    ],
-
-    resolve: {
-      alias: [
-        {
-          find: '@northern.tech/store',
-          replacement: path.resolve(__dirname, 'src/js/store')
+        env: {
+          BABEL_ENV: 'test',
+          NODE_ENV: 'test',
+          PUBLIC_URL: '',
+          TZ: 'UTC'
         },
-        {
-          find: '@northern.tech/common-ui',
-          replacement: path.resolve(__dirname, 'src/js/common-ui')
-        }
-      ]
-    },
-    server: {
-      port: 80,
-      middlewareMode: false
-    },
-    test: {
-      coverage: {
-        reporter: ['json', 'lcov'],
-        reportsDirectory: 'coverage'
-      },
-      env: {
-        BABEL_ENV: 'test',
-        NODE_ENV: 'test',
-        PUBLIC_URL: '',
-        TZ: 'UTC'
-      },
-      environment: 'jsdom',
-      globals: true,
-      setupFiles: './tests/setupTests.jsx',
-      fakeTimers: {
-        toFake: ['setTimeout', 'clearTimeout', 'Date']
-      },
-      pool: 'threads',
-      poolOptions: {
-        threads: {
-          minThreads: threadCount,
-          maxThreads: threadCount,
-          useAtomics: true
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: './tests/setupTests.jsx',
+        fakeTimers: {
+          toFake: ['setTimeout', 'clearTimeout', 'Date']
         }
       }
-    }
-  } as UserWorkspaceConfig;
-});
+    }) as UserWorkspaceConfig
+);
