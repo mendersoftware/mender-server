1f5f43f83c97808fd08ac29388947d88ecda255c
Manuel Zedel <manuel.zedel@northern.tech>
2025-06-23 14:36:27 +0200

    refactor(gui): let chart adding + editing rely on rhf
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- frontend/src/js/components/dashboard/widgets/ChartAddition.tsx
+++ frontend/src/js/components/dashboard/widgets/ChartAddition.tsx
@@ -11,7 +11,8 @@
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
 
 import { Add as AddIcon } from '@mui/icons-material';
 import { Button, FormControl, IconButton, InputLabel, ListSubheader, MenuItem, Select, iconButtonClasses, selectClasses, svgIconClasses } from '@mui/material';
@@ -20,6 +21,7 @@ import { makeStyles } from 'tss-react/mui';
 import Confirm from '@northern.tech/common-ui/Confirm';
 import EnterpriseNotification from '@northern.tech/common-ui/EnterpriseNotification';
 import { InfoHintContainer } from '@northern.tech/common-ui/InfoHint';
+import Form from '@northern.tech/common-ui/forms/Form';
 import { HELPTOOLTIPS } from '@northern.tech/common-ui/helptips/HelpTooltips';
 import { MenderHelpTooltip } from '@northern.tech/common-ui/helptips/MenderTooltip';
 import { BENEFITS, chartTypes, emptyChartSelection } from '@northern.tech/store/constants';
@@ -67,110 +69,122 @@ export const Header = ({ chartType }) => {
   );
 };
 
-const GroupSelect = ({ groups, selection, setSelection }) => (
-  <FormControl className="margin-top-none">
-    <InputLabel id="group-select-label">Device group</InputLabel>
-    <Select labelId="group-select-label" label="Device group" value={selection.group || true} onChange={e => setSelection({ group: e.target.value })}>
-      <MenuItem value={true}>
-        <em>All Devices</em>
-      </MenuItem>
-      {Object.keys(groups).map(group => (
-        <MenuItem key={group} value={group}>
-          {group}
-        </MenuItem>
-      ))}
-    </Select>
-  </FormControl>
-);
+const GroupSelect = ({ groups, name }) => {
+  const { control } = useFormContext();
+  return (
+    <FormControl className="margin-top-none">
+      <InputLabel id="group-select-label" shrink>
+        Device group
+      </InputLabel>
+      <Controller
+        name={name}
+        control={control}
+        defaultValue=""
+        render={({ field }) => (
+          <Select labelId="group-select-label" displayEmpty label="Device group" {...field}>
+            <MenuItem value="">
+              <em>All Devices</em>
+            </MenuItem>
+            {Object.keys(groups).map(group => (
+              <MenuItem key={group} value={group}>
+                {group}
+              </MenuItem>
+            ))}
+          </Select>
+        )}
+      />
+    </FormControl>
+  );
+};
 
 const themeSpacing = 8;
 const basePadding = 2 * themeSpacing;
 const getIndentation = level => ({ paddingLeft: basePadding + level * themeSpacing });
 
-const SoftwareSelect = ({ selection, setSelection, software }) => (
-  <FormControl className="margin-top-none">
-    <InputLabel shrink id="software-select-label">
-      Software
-    </InputLabel>
-    <Select
-      labelId="software-select-label"
-      label="Software"
-      displayEmpty
-      value={selection.software}
-      onBlur={undefined}
-      onChange={e => setSelection({ software: e.target.value })}
-    >
-      {software.map(({ subheader, title, value, nestingLevel }) =>
-        subheader ? (
-          <ListSubheader key={value} style={getIndentation(nestingLevel)}>
-            {subheader}
-          </ListSubheader>
-        ) : (
-          <MenuItem key={value} style={getIndentation(nestingLevel)} value={value}>
-            {title}
-          </MenuItem>
-        )
-      )}
-    </Select>
-  </FormControl>
-);
+const SoftwareSelect = ({ software, name }) => {
+  const { control } = useFormContext();
+  return (
+    <FormControl className="margin-top-none">
+      <InputLabel shrink id="software-select-label">
+        Software
+      </InputLabel>
+      <Controller
+        name={name}
+        control={control}
+        defaultValue=""
+        render={({ field }) => (
+          <Select labelId="software-select-label" label="Software" displayEmpty {...field}>
+            {software.map(({ subheader, title, value, nestingLevel }) =>
+              subheader ? (
+                <ListSubheader key={value} style={getIndentation(nestingLevel)}>
+                  {subheader}
+                </ListSubheader>
+              ) : (
+                <MenuItem key={value} style={getIndentation(nestingLevel)} value={value}>
+                  {title}
+                </MenuItem>
+              )
+            )}
+          </Select>
+        )}
+      />
+    </FormControl>
+  );
+};
 
-const ChartSelect = ({ classes, selection, setSelection }) => (
-  <div>
-    {Object.values(chartTypes).map(type => {
-      const { Icon, key } = type;
-      return (
-        <IconButton
-          className={`${classes.iconButton} ${selection.chartType === key ? 'selected' : ''}`}
-          key={key}
-          size="small"
-          onClick={() => setSelection({ chartType: key })}
-        >
-          <Icon fontSize="small" />
-        </IconButton>
-      );
-    })}
-  </div>
-);
+const ChartSelect = ({ classes, name }) => {
+  const { control, watch } = useFormContext();
+  const selectedType = watch(name);
+  return (
+    <Controller
+      name={name}
+      control={control}
+      defaultValue={Object.keys(chartTypes)[0]}
+      render={({ field: { onChange } }) => (
+        <div>
+          {Object.values(chartTypes).map(type => {
+            const { Icon, key } = type;
+            return (
+              <IconButton className={`${classes.iconButton} ${selectedType === key ? 'selected' : ''}`} key={key} size="small" onClick={() => onChange(key)}>
+                <Icon fontSize="small" />
+              </IconButton>
+            );
+          })}
+        </div>
+      )}
+    />
+  );
+};
 
 const chartOptions = [
   { key: 'software', title: 'Software', Selector: SoftwareSelect },
   { key: 'group', title: 'Device group', Selector: GroupSelect },
-  { key: 'type', title: 'Display', Selector: ChartSelect }
+  { key: 'chartType', title: 'Display', Selector: ChartSelect }
 ];
 
 export const ChartEditWidget = ({ groups, onSave, onCancel, selection: selectionProp = {}, software = [] }) => {
-  const [selection, setSelection] = useState({ ...emptyChartSelection, ...selectionProp });
   const { classes } = useStyles();
 
-  const addCurrentSelection = useCallback(
-    () => onSave({ ...emptyChartSelection, ...selection, group: typeof selection.group === 'string' ? selection.group : null }),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [JSON.stringify(selection), onSave]
-  );
-
-  const onSelectionChange = changedSelection => setSelection(current => ({ ...current, ...changedSelection }));
-
   return (
-    <div className="widget chart-widget">
-      <Header chartType={selection.chartType} />
+    <Form
+      defaultValues={emptyChartSelection}
+      initialValues={selectionProp}
+      onSubmit={onSave}
+      handleCancel={onCancel}
+      showButtons
+      submitLabel="Save"
+      className="widget chart-widget"
+    >
+      <Header chartType={emptyChartSelection.chartType} />
       <div className={classes.formWrapper}>
         {chartOptions.map(({ key, title, Selector }) => (
           <React.Fragment key={key}>
             <div>{title}</div>
-            <Selector classes={classes} groups={groups} software={software} selection={selection} setSelection={onSelectionChange} />
+            <Selector classes={classes} groups={groups} software={software} name={key} />
           </React.Fragment>
         ))}
       </div>
-      <div className={classes.buttonWrapper}>
-        <Button className={classes.button} size="small" onClick={onCancel}>
-          Cancel
-        </Button>
-        <Button className={classes.button} size="small" onClick={addCurrentSelection} variant="contained" disabled={!selection}>
-          Save
-        </Button>
-      </div>
-    </div>
+    </Form>
   );
 };
 
