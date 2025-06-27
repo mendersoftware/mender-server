2ac56be82cc7c180526a5c902fccd3f46f0fffbd
Alf-Rune Siqveland <alf.rune@northern.tech>
2025-06-27 09:27:34 +0200

    refactor(pkg): Create event aware Limiter and refactor redis limiter
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- backend/pkg/rate/limit.go
+++ backend/pkg/rate/limit.go
@@ -11,7 +11,17 @@ import (
 // but extending the interface with the ability to expose internal errors.
 type Limiter interface {
 	Reserve(ctx context.Context) (Reservation, error)
-	Tokens(ctx context.Context) (uint64, error)
+}
+
+type LimiterFunc func(context.Context) (Reservation, error)
+
+func (f LimiterFunc) Reserve(ctx context.Context) (Reservation, error) {
+	return f(ctx)
+}
+
+// EventLimiter allows grouping limiters by eventID.
+type EventLimiter interface {
+	ReserveEvent(ctx context.Context, eventID string) (Reservation, error)
 }
 
 // Reservation is a point-in-time reservation of a ratelimit token. If the token
@@ -21,7 +31,7 @@ type Limiter interface {
 type Reservation interface {
 	OK() bool
 	Delay() time.Duration
-	Tokens() uint64
+	Tokens() int64
 }
 
 type limiter rate.Limiter
@@ -38,22 +48,14 @@ func (lim *limiter) Reserve(context.Context) (Reservation, error) {
 		time:        now,
 	}
 	if res.OK() {
-		res.tokens = uint64(goLimiter.TokensAt(now))
+		res.tokens = int64(goLimiter.TokensAt(now))
 	}
 	return res, nil
 }
 
-func (lim *limiter) Tokens(context.Context) (uint64, error) {
-	goLimiter := (*rate.Limiter)(lim)
-	if tokens := goLimiter.Tokens(); tokens > 0 {
-		return uint64(tokens), nil
-	}
-	return 0, nil
-}
-
 type reservation struct {
 	time        time.Time
-	tokens      uint64
+	tokens      int64
 	reservation *rate.Reservation
 }
 
@@ -65,6 +67,6 @@ func (r *reservation) Delay() time.Duration {
 	return r.reservation.DelayFrom(r.time)
 }
 
-func (r *reservation) Tokens() uint64 {
+func (r *reservation) Tokens() int64 {
 	return r.tokens
 }
