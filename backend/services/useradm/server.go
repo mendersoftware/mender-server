411f5f1071004c8788160159bc11aee17d06b946
Alf-Rune Siqveland <alf.rune@northern.tech>
2025-06-30 14:19:59 +0200

    refactor(useradm): Create utility function to load JWT PKI
    ... to lower cyclomatic complexity for main.RunServer.
    
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- backend/services/useradm/server.go
+++ backend/services/useradm/server.go
@@ -42,86 +42,13 @@ import (
 )
 
 func RunServer(c config.Reader) error {
-
 	l := log.New(log.Ctx{})
 
 	authorizer := &SimpleAuthz{}
 
-	// let's now go through all the existing keys and load them
-	jwtHandlers, err := addPrivateKeys(
-		l,
-		filepath.Dir(c.GetString(SettingServerPrivKeyPath)),
-		c.GetString(SettingServerPrivKeyFileNamePattern),
-	)
+	jwtHandlers, jwtFallbackHandler, err := loadJWTHandlers(c, l)
 	if err != nil {
-		return err
-	}
-
-	// the handler for keyId equal 0 is the one associated with the
-	// SettingServerPrivKeyPathDefault key. it is the one serving all the previously
-	// issued tokens (before the kid introduction in the JWTs)
-	defaultHandler, err := jwt.NewJWTHandler(
-		SettingServerPrivKeyPathDefault,
-		c.GetString(SettingServerPrivKeyFileNamePattern),
-	)
-	if err == nil && defaultHandler != nil {
-		// the key with id 0 is by default the default one. this allows
-		// to support tokens without "kid" in the header
-		// it is possible, that you rotated the default key, in which case you have to
-		// set USERADM_SERVER_PRIV_KEY_PATH=/etc/useradm/rsa/private.id.2048.pem
-		// where private.id.2048.pem is the new key, with new id. the new one will by default
-		// be used to issue new tokens, while any other token which has id that we have
-		// will be authorized against its matching key (by id from "kid" in JWT header)
-		// or which does not have "kid" will be authorized against the key with id 0.
-		// in other words: the key with id 0 (if not present as private.id.0.pem)
-		// is the default one, and all the JWT with no "kid" in headers are being
-		// checked against it.
-		jwtHandlers[common.KeyIdZero] = defaultHandler
-	}
-
-	// if the default path is different from the currently set key path
-	// we still have not loaded this key. this happens when the key rotation took place,
-	// someone exported USERADM_SERVER_PRIV_KEY_PATH=path-to-a-new-key and this key
-	// now will serve all. if we do not have this, the Login will fall back to the keyId
-	// from the filename and either use the KeyIdZero key or fail to find the key to issue a
-	// token if the one set in USERADM_SERVER_PRIV_KEY_PATH does have id in the filename
-	// (but does not exist because we have not loaded it)
-	// this also means that careless setting of USERADM_SERVER_PRIV_KEY_PATH to a key that does
-	// not match the SettingServerPrivKeyFileNamePattern will result in
-	// KeyIdZero handler overwrite and lack of back support for tokens signed by it.
-	if c.GetString(SettingServerPrivKeyPath) != SettingServerPrivKeyPathDefault {
-		defaultHandler, err = jwt.NewJWTHandler(
-			c.GetString(SettingServerPrivKeyPath),
-			c.GetString(SettingServerPrivKeyFileNamePattern),
-		)
-		if err == nil && defaultHandler != nil {
-			keyId := common.KeyIdFromPath(
-				c.GetString(SettingServerPrivKeyPath),
-				c.GetString(SettingServerPrivKeyFileNamePattern),
-			)
-			if keyId == common.KeyIdZero {
-				l.Warnf(
-					"currently set private key %s either does not match %s pattern"+
-						" or has explicitly set id=0. we are overridding the default"+
-						" private key handler with id=0",
-					c.GetString(SettingServerPrivKeyPath),
-					c.GetString(SettingServerPrivKeyFileNamePattern),
-				)
-			}
-			jwtHandlers[keyId] = defaultHandler
-		}
-	}
-
-	var jwtFallbackHandler jwt.Handler
-	fallback := c.GetString(SettingServerFallbackPrivKeyPath)
-	if err == nil && fallback != "" {
-		jwtFallbackHandler, err = jwt.NewJWTHandler(
-			fallback,
-			c.GetString(SettingServerPrivKeyFileNamePattern),
-		)
-	}
-	if err != nil {
-		return err
+		return fmt.Errorf("error loading JWT handlers: %w", err)
 	}
 
 	db, err := mongo.GetDataStoreMongo(dataStoreMongoConfigFromAppConfig(c))
@@ -208,11 +135,13 @@ func RunServer(c config.Reader) error {
 	return nil
 }
 
-func addPrivateKeys(
+func loadJWTHandlers(
+	c config.Reader,
 	l *log.Logger,
-	privateKeysDirectory string,
-	privateKeyPattern string,
-) (handlers map[int]jwt.Handler, err error) {
+) (handlers map[int]jwt.Handler, fallbackHandler jwt.Handler, err error) {
+	privateKeysDirectory := filepath.Dir(c.GetString(SettingServerPrivKeyPath))
+	privateKeyPattern := c.GetString(SettingServerPrivKeyFileNamePattern)
+
 	files, err := os.ReadDir(privateKeysDirectory)
 	if err != nil {
 		return
@@ -236,5 +165,72 @@ func addPrivateKeys(
 			handlers[keyId] = handler
 		}
 	}
-	return handlers, nil
+
+	// the handler for keyId equal 0 is the one associated with the
+	// SettingServerPrivKeyPathDefault key. it is the one serving all the previously
+	// issued tokens (before the kid introduction in the JWTs)
+	defaultHandler, err := jwt.NewJWTHandler(
+		SettingServerPrivKeyPathDefault,
+		c.GetString(SettingServerPrivKeyFileNamePattern),
+	)
+	if err == nil && defaultHandler != nil {
+		// the key with id 0 is by default the default one. this allows
+		// to support tokens without "kid" in the header
+		// it is possible, that you rotated the default key, in which case you have to
+		// set USERADM_SERVER_PRIV_KEY_PATH=/etc/useradm/rsa/private.id.2048.pem
+		// where private.id.2048.pem is the new key, with new id. the new one will by default
+		// be used to issue new tokens, while any other token which has id that we have
+		// will be authorized against its matching key (by id from "kid" in JWT header)
+		// or which does not have "kid" will be authorized against the key with id 0.
+		// in other words: the key with id 0 (if not present as private.id.0.pem)
+		// is the default one, and all the JWT with no "kid" in headers are being
+		// checked against it.
+		handlers[common.KeyIdZero] = defaultHandler
+	}
+
+	// if the default path is different from the currently set key path
+	// we still have not loaded this key. this happens when the key rotation took place,
+	// someone exported USERADM_SERVER_PRIV_KEY_PATH=path-to-a-new-key and this key
+	// now will serve all. if we do not have this, the Login will fall back to the keyId
+	// from the filename and either use the KeyIdZero key or fail to find the key to issue a
+	// token if the one set in USERADM_SERVER_PRIV_KEY_PATH does have id in the filename
+	// (but does not exist because we have not loaded it)
+	// this also means that careless setting of USERADM_SERVER_PRIV_KEY_PATH to a key that does
+	// not match the SettingServerPrivKeyFileNamePattern will result in
+	// KeyIdZero handler overwrite and lack of back support for tokens signed by it.
+	if c.GetString(SettingServerPrivKeyPath) != SettingServerPrivKeyPathDefault {
+		defaultHandler, err = jwt.NewJWTHandler(
+			c.GetString(SettingServerPrivKeyPath),
+			c.GetString(SettingServerPrivKeyFileNamePattern),
+		)
+		if err == nil && defaultHandler != nil {
+			keyId := common.KeyIdFromPath(
+				c.GetString(SettingServerPrivKeyPath),
+				c.GetString(SettingServerPrivKeyFileNamePattern),
+			)
+			if keyId == common.KeyIdZero {
+				l.Warnf(
+					"currently set private key %s either does not match %s pattern"+
+						" or has explicitly set id=0. we are overridding the default"+
+						" private key handler with id=0",
+					c.GetString(SettingServerPrivKeyPath),
+					c.GetString(SettingServerPrivKeyFileNamePattern),
+				)
+			}
+			handlers[keyId] = defaultHandler
+		}
+	}
+
+	fallback := c.GetString(SettingServerFallbackPrivKeyPath)
+	if err == nil && fallback != "" {
+		fallbackHandler, err = jwt.NewJWTHandler(
+			fallback,
+			c.GetString(SettingServerPrivKeyFileNamePattern),
+		)
+	}
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return handlers, fallbackHandler, nil
 }
