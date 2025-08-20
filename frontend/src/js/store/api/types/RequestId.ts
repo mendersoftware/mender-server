c06eb5e0c589a47507fb93935fe8aed572e0ed94
Manuel Zedel <manuel.zedel@northern.tech>
2025-08-20 14:06:52 +0200

    refactor(gui): made use of store + testing package in codebase
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- frontend/src/js/store/api/types/RequestId.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-/**
- * A request identification
- */
-export type RequestId = string;
