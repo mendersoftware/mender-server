5116509e0c38199c7ae7b8463c6a3f92b4508763
Manuel Zedel <manuel.zedel@northern.tech>
2025-08-20 14:06:52 +0200

    refactor(gui): made use of store + testing package in codebase
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- frontend/src/js/store/api/types/Authentication.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-/**
- * The token in base64-encoded form.
- */
-export type Authentication = string;
--- frontend/src/js/store/api/types/UpdateTypes.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-/**
- * Update types as present in the images.
- */
-export type UpdateTypes = Array<string>;
