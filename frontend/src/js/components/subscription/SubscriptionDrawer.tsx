ec758ad6530f1ec16f10b19fa93eebe2909c109a
Mikita Pilinka <mikita.pilinka@northern.tech>
2025-09-02 18:44:55 +0200

    feat(gui): separated billing details edit & cc edit
    Ticket: MEN-8644
    
    Signed-off-by: Mikita Pilinka <mikita.pilinka@northern.tech>

--- frontend/src/js/components/subscription/SubscriptionDrawer.tsx
+++ frontend/src/js/components/subscription/SubscriptionDrawer.tsx
@@ -11,10 +11,10 @@
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
-import { ErrorOutline as ErrorOutlineIcon } from '@mui/icons-material';
+import { CheckCircleOutlined as CheckCircleOutlinedIcon, ErrorOutline as ErrorOutlineIcon } from '@mui/icons-material';
 import { Alert, Button, CircularProgress, Divider, Drawer, Typography, buttonClasses } from '@mui/material';
 import { makeStyles } from 'tss-react/mui';
 
@@ -25,7 +25,7 @@ import Form from '@northern.tech/common-ui/forms/Form';
 import { Address } from '@northern.tech/store/api/types/Address';
 import { AvailableAddon, PLANS, Plan } from '@northern.tech/store/constants';
 import { Organization } from '@northern.tech/store/organizationSlice/types';
-import { getBillingProfile, getCurrentUser, getSubscription } from '@northern.tech/store/selectors';
+import { getBillingProfile, getCard, getCurrentUser, getSubscription } from '@northern.tech/store/selectors';
 import { useAppDispatch } from '@northern.tech/store/store';
 import {
   confirmCardUpdate,
@@ -38,6 +38,7 @@ import {
 
 import CardSection from '../settings/CardSection';
 import { PlanExpandedForm } from '../settings/PlanExpandedForm';
+import { CardDetails } from '../settings/organization/Billing';
 import { BillingDetails } from '../settings/organization/BillingDetails';
 import OrganizationPaymentSettings from '../settings/organization/OrganizationPaymentSettings';
 import { PreviewPrice } from './SubscriptionPage';
@@ -74,6 +75,7 @@ const emptyAddress: Address = { city: '', country: '', line1: '', postal_code: '
 export const SubscriptionDrawer = (props: SubscriptionDrawerProps) => {
   const { onClose, previewPrice, order, isTrial, plan: selectedPlan, organization, currentPlanId } = props;
   const { email } = useSelector(getCurrentUser);
+  const card = useSelector(getCard);
   const billing = useSelector(getBillingProfile);
   const currentSubscription = useSelector(getSubscription);
   const initialValues = { email, name: organization?.name || '', line1: '', state: '', city: '', postal_code: '', country: '' };
@@ -83,17 +85,12 @@ export const SubscriptionDrawer = (props: SubscriptionDrawerProps) => {
   const [nextPayment, setNextPayment] = useState(0);
   const [updatingCard, setUpdatingCard] = useState(false);
   const dispatch = useAppDispatch();
-  const formSubmitRef = useRef<() => void | null>(null);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [billingSaved, setBillingSaved] = useState(false);
 
   const { classes } = useStyles();
 
-  const handleUpgrade = () => {
-    if (formSubmitRef.current) {
-      formSubmitRef.current();
-    }
-  };
   const onInitEditProfile = () => {
     setIsEdit(true);
     setFormInitialValues({ ...(billing.address || emptyAddress), name: billing.name, email: billing.email });
@@ -114,7 +111,9 @@ export const SubscriptionDrawer = (props: SubscriptionDrawerProps) => {
     if (isEdit) {
       await dispatch(editBillingProfile({ billingProfile: billing_profile }));
     } else if (isTrial) {
-      await dispatch(createBillingProfile({ billingProfile: billing_profile }));
+      await dispatch(createBillingProfile({ billingProfile: billing_profile }))
+        .unwrap()
+        .then(() => setBillingSaved(true));
     }
     setIsEdit(false);
   };
@@ -149,7 +148,7 @@ export const SubscriptionDrawer = (props: SubscriptionDrawerProps) => {
       />
     </div>
   );
-
+  const cardDetailsDisabled = isTrial && !billing;
   return (
     <Drawer anchor="right" open={true} PaperProps={{ style: { minWidth: '50vw' } }}>
       <DrawerTitle title={currentSubscription ? `Upgrade your subscription` : `Subscribe to Mender ${selectedPlan.name}`} onClose={onClose} />
@@ -163,7 +162,6 @@ export const SubscriptionDrawer = (props: SubscriptionDrawerProps) => {
       {isEdit || (isTrial && !billing) ? (
         <Form
           classes={classes}
-          submitRef={formSubmitRef}
           onSubmit={handleBillingProfileEdit}
           handleCancel={billing && (() => setIsEdit(false))}
           defaultValues={formInitialValues}
@@ -176,36 +174,57 @@ export const SubscriptionDrawer = (props: SubscriptionDrawerProps) => {
         </Form>
       ) : (
         <>
-          <Typography className="margin-top-small" variant="subtitle1">
-            Billing details
+          <Typography className="margin-top-small margin-bottom-x-small" variant="subtitle1">
+            Your billing details
           </Typography>
-          <BillingDetails setChangeBilling={onInitEditProfile} hideCard={isTrial} />
+          <BillingDetails setChangeBilling={onInitEditProfile} hideCard editDisabled={updatingCard} />
+
+          {billingSaved && (
+            <Alert className="margin-top-large" icon={<CheckCircleOutlinedIcon />}>
+              Billing details saved
+            </Alert>
+          )}
         </>
       )}
-      {isEdit ? (
-        <OrganizationPaymentSettings
-          className={classes.formWrapper}
-          onComplete={handleUpgrade}
-          updatingCard={updatingCard}
-          setUpdatingCard={setUpdatingCard}
-          isValid={isValid}
-        />
+      {isTrial ? (
+        <div style={{ maxWidth: 600 }}>
+          <Typography variant="subtitle1" color={cardDetailsDisabled ? 'textDisabled' : 'textPrimary'} className="margin-top-large margin-bottom-none">
+            Card details
+          </Typography>
+          <CardSection
+            organization={organization}
+            onCardConfirmed={onCardConfirmed}
+            onSubmit={() => dispatch(startCardUpdate()).unwrap()}
+            summary={summary}
+            isSignUp
+            isValid={!isEdit}
+            disabled={cardDetailsDisabled}
+          />
+        </div>
       ) : (
-        billing &&
-        isTrial && (
-          <div className={classes.formWrapper}>
-            <h4 className="margin-top margin-bottom-none">Card Details</h4>
-            <CardSection
-              organization={organization}
-              onCardConfirmed={onCardConfirmed}
-              onSubmit={() => dispatch(startCardUpdate()).unwrap()}
-              summary={summary}
-              isSignUp
+        <div>
+          <Typography className="margin-top-large margin-bottom-x-small" variant="subtitle1">
+            Card details
+          </Typography>
+          {updatingCard ? (
+            <OrganizationPaymentSettings
+              className={classes.formWrapper}
+              updatingCard={updatingCard}
+              setUpdatingCard={setUpdatingCard}
+              isValid={isValid}
+              omitHeader
             />
-          </div>
-        )
+          ) : (
+            <>
+              <CardDetails card={card} />
+              <Button disabled={isEdit} variant="outlined" className="margin-top-x-small" onClick={() => setUpdatingCard(true)} size="medium">
+                Change card
+              </Button>
+            </>
+          )}
+        </div>
       )}
-      {(!isTrial || (isTrial && !billing)) && summary}
+      {!isTrial && summary}
       {nextPayment > 0 && currentSubscription && currentPlanId ? (
         <div className={classes.formWrapper}>
           <p>
