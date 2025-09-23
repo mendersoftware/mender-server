c47c66a9e7ed191cd7a4a176fecf44a3d8f68cd3
Manuel Zedel <manuel.zedel@northern.tech>
2025-09-23 15:52:06 +0200

    chore(gui): fixed lint issues after dependency updates
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- frontend/src/js/store/devicesSlice/thunks.test.tsx
+++ frontend/src/js/store/devicesSlice/thunks.test.tsx
@@ -12,7 +12,6 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 // @ts-nocheck
-
 /*eslint import/namespace: ['error', { allowComputed: true }]*/
 import React from 'react';
 import { Link } from 'react-router-dom';
