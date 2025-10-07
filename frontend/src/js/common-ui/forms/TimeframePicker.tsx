59cac137acd21daf69ea8f54dddb6f1fde0da7b2
Manuel Zedel <manuel.zedel@northern.tech>
2025-10-07 13:20:41 +0200

    feat(gui): let timeframe filter elements also show context dependent notifications
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- frontend/src/js/common-ui/forms/TimeframePicker.tsx
+++ frontend/src/js/common-ui/forms/TimeframePicker.tsx
@@ -14,10 +14,25 @@
 import { useEffect, useState } from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
 
-import { useTheme } from '@mui/material';
+import { Typography, useTheme } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { makeStyles } from 'tss-react/mui';
 
 import dayjs from 'dayjs';
+
+const useStyles = makeStyles()(theme => ({
+  container: {
+    alignItems: 'start',
+    width: 'min-content'
+  },
+  inputs: {
+    gap: theme.spacing(2),
+    flexWrap: 'wrap',
+    [theme.breakpoints.up('md')]: {
+      flexWrap: 'nowrap'
+    }
+  }
+}));
 
 const ensureStartOfDay = date => {
   const momentDate = typeof date === 'string' ? dayjs(date.replace('Z', '')) : dayjs(date);
@@ -29,10 +44,27 @@ const ensureEndOfDay = date => {
   return `${momentDate.format().split('T')[0]}T23:59:59.999`;
 };
 
-export const TimeframePicker = ({ tonight: propsTonight }) => {
+const no = () => false;
+
+interface HasHelptextParams {
+  endDate: string;
+  startDate: string;
+}
+
+export const TimeframePicker = ({
+  hasHelperText = no,
+  helperText,
+  tonight: propsTonight
+}: {
+  hasHelperText?: (params: HasHelptextParams) => boolean;
+  helperText?: string;
+  tonight: string;
+}) => {
   const [tonight] = useState(dayjs(propsTonight));
   const [maxStartDate, setMaxStartDate] = useState(tonight);
   const [minEndDate, setMinEndDate] = useState(tonight);
+  const [showsHelptext, setShowsHelptext] = useState(false);
+  const { classes } = useStyles();
 
   const theme = useTheme();
   const isNextTheme = !theme.palette.background.lightgrey;
@@ -61,60 +93,71 @@ export const TimeframePicker = ({ tonight: propsTonight }) => {
     setMaxStartDate(dayjs(endDate));
   }, [endDate, getValues, setValue]);
 
+  useEffect(() => {
+    setShowsHelptext(hasHelperText({ startDate, endDate }));
+  }, [endDate, hasHelperText, startDate]);
+
   const handleChangeStartDate = date => ensureStartOfDay(date);
 
   const handleChangeEndDate = date => ensureEndOfDay(date);
 
   return (
-    <div className="flexbox" style={{ flexWrap: 'wrap', gap: 15 }}>
-      <Controller
-        name="startDate"
-        control={control}
-        render={({ field: { onChange, value } }) => (
-          <DatePicker
-            disableFuture
-            slotProps={{
-              textField: props => ({
-                size: isNextTheme ? 'small' : 'medium',
-                inputProps: {
-                  ...props.inputProps,
-                  'aria-label': 'From'
-                }
-              })
-            }}
-            format="YYYY-MM-DD"
-            yearsOrder="desc"
-            label="From"
-            maxDate={maxStartDate}
-            onChange={e => onChange(handleChangeStartDate(e))}
-            value={value ? dayjs(value) : null}
-          />
-        )}
-      />
-      <Controller
-        name="endDate"
-        control={control}
-        render={({ field: { onChange, value } }) => (
-          <DatePicker
-            disableFuture
-            slotProps={{
-              textField: props => ({
-                size: isNextTheme ? 'small' : 'medium',
-                inputProps: {
-                  ...props.inputProps,
-                  'aria-label': 'To'
-                }
-              })
-            }}
-            format="YYYY-MM-DD"
-            yearsOrder="desc"
-            label="To"
-            minDate={minEndDate}
-            onChange={e => onChange(handleChangeEndDate(e))}
-            value={value ? dayjs(value) : dayjs()}
-          />
-        )}
-      />
+    <div className={`flexbox column ${classes.container}`}>
+      <div className={`flexbox ${classes.inputs}`}>
+        <Controller
+          name="startDate"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <DatePicker
+              disableFuture
+              slotProps={{
+                textField: props => ({
+                  size: isNextTheme ? 'small' : 'medium',
+                  inputProps: {
+                    ...props.inputProps,
+                    'aria-label': 'From'
+                  }
+                })
+              }}
+              format="YYYY-MM-DD"
+              yearsOrder="desc"
+              label="From"
+              maxDate={maxStartDate}
+              onChange={e => onChange(handleChangeStartDate(e))}
+              value={value ? dayjs(value) : null}
+            />
+          )}
+        />
+        <Controller
+          name="endDate"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <DatePicker
+              disableFuture
+              slotProps={{
+                textField: props => ({
+                  size: isNextTheme ? 'small' : 'medium',
+                  inputProps: {
+                    ...props.inputProps,
+                    'aria-label': 'To'
+                  }
+                })
+              }}
+              format="YYYY-MM-DD"
+              yearsOrder="desc"
+              label="To"
+              minDate={minEndDate}
+              onChange={e => onChange(handleChangeEndDate(e))}
+              value={value ? dayjs(value) : dayjs()}
+            />
+          )}
+        />
+      </div>
+      {showsHelptext && (
+        <Typography className="margin-left-small margin-top-x-small" color="info" variant="body2">
+          {helperText}
+        </Typography>
+      )}
     </div>
   );
 };
