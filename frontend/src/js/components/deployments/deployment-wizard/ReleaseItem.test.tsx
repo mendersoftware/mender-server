9efb3278ab8395a9f556ae98ed076f6dba0e7e2f
Mikita Pilinka <mikita.pilinka@northern.tech>
2025-12-16 14:55:37 +0100

    test(gui): added release filter unit tests
    Ticket: MEN-8669
    
    Signed-off-by: Mikita Pilinka <mikita.pilinka@northern.tech>

---
+++ frontend/src/js/components/deployments/deployment-wizard/ReleaseItem.test.tsx
@@ -0,0 +1,29 @@
+// Copyright 2025 Northern.tech AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+import { defaultState, render } from '@/testUtils';
+import { undefineds } from '@northern.tech/testing/mockData';
+import { vi } from 'vitest';
+
+import { ReleaseItem } from './ReleaseItem';
+
+describe('ReleaseArtifactItem Component', () => {
+  it('renders correctly', async () => {
+    const release = Object.values(defaultState.releases.byId)[0];
+
+    const { baseElement } = render(<ReleaseItem release={release} onClick={vi.fn} />);
+    const view = baseElement.firstChild;
+    expect(view).toMatchSnapshot();
+    expect(view).toEqual(expect.not.stringMatching(undefineds));
+  });
+});
