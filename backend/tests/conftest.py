9f937f1a91796ec01da9e07f2ab9f699ef0c6fb9
Alf-Rune Siqveland <alf.rune@northern.tech>
2025-12-29 21:55:35 +0100

    test: configure generated client for test environment
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -11,11 +11,35 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
+
+import os
+
 import pytest
+import urllib3
+
+from mender_client import ApiClient, Configuration
 
 # See https://docs.pytest.org/en/latest/writing_plugins.html#assertion-rewriting
 pytest.register_assert_rewrite("testutils")
 
-from requests.packages import urllib3
 
 urllib3.disable_warnings()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--mender-url",
+        action="store",
+        default=os.environ["TEST_MENDER_URL"]
+        if "TEST_MENDER_URL" in os.environ
+        else "https://docker.mender.io",
+        help="Address for host hosting deviceconnect API (env: TEST_MENDER_URL)",
+    )
+
+
+def pytest_configure(config: pytest.Config):
+    mender_url = config.getoption("mender_url")
+    client_config = Configuration(host=mender_url)
+    client_config.verify_ssl = False
+    Configuration.set_default(client_config)
+    ApiClient.set_default(ApiClient(client_config))
