d46851d9376a85d9af7831dd450a3eb8a9da6ae1
Mikita Pilinka <mikita.pilinka@northern.tech>
2026-01-21 14:57:19 +0100

    feat(gui): adjusted 2fa form to align with new design
    Ticket: MEN-9095
    
    Signed-off-by: Mikita Pilinka <mikita.pilinka@northern.tech>

--- frontend/src/js/components/settings/user-management/twofactorauth-steps/AuthSetup.tsx
+++ frontend/src/js/components/settings/user-management/twofactorauth-steps/AuthSetup.tsx
@@ -13,15 +13,29 @@
 //    limitations under the License.
 import { useEffect, useRef, useState } from 'react';
 
-import { CheckCircle as CheckCircleIcon } from '@mui/icons-material';
-import { Button } from '@mui/material';
+import { Button, Typography } from '@mui/material';
+import { makeStyles } from 'tss-react/mui';
 
 import Loader from '@northern.tech/common-ui/Loader';
 import Form from '@northern.tech/common-ui/forms/Form';
 import TextInput from '@northern.tech/common-ui/forms/TextInput';
 import { twoFAStates } from '@northern.tech/store/constants';
 
-export const AuthSetup = ({ currentUser, handle2FAState, has2FA, onClose, qrImage, verify2FA }) => {
+import { notificationMap } from '../SelfUserManagement';
+
+const useStyles = makeStyles()(theme => ({
+  orderedList: {
+    marginBottom: 0,
+    marginTop: theme.spacing(),
+    maxWidth: '550px'
+  },
+  qrImg: { maxWidth: '188px' },
+  codeInput: { maxWidth: '220px', marginTop: theme.spacing(1) },
+  buttonWrapper: { justifyContent: 'flex-end', flexDirection: 'row-reverse', marginTop: theme.spacing(2) },
+  cancelButton: { marginLeft: theme.spacing() }
+}));
+
+export const AuthSetup = ({ currentUser, handle2FAState, has2FA, onClose, qrImage, verify2FA, setShowNotice }) => {
   const current2FA = useRef(has2FA);
   const [validated2fa, setValidated2fa] = useState(false);
   const [validating2fa, setValidating2fa] = useState(false);
@@ -47,71 +61,58 @@ export const AuthSetup = ({ currentUser, handle2FAState, has2FA, onClose, qrImag
       window.removeEventListener('beforeunload', onUnload);
     };
   }, [handle2FAState, qrImage, validated2fa]);
+  const { classes } = useStyles();
 
   const validate2faSetup = formData => {
     setValidating2fa(true);
     formData.email = currentUser.email;
     return verify2FA(formData)
-      .then(() => setValidated2fa(true))
+      .unwrap()
+      .then(() => {
+        setValidated2fa(true);
+        setShowNotice(notificationMap.enabled2fa);
+        onClose();
+      })
       .catch(() => setValidated2fa(false))
       .finally(() => setValidating2fa(false));
   };
 
   return (
-    <div className="margin-top">
+    <div className="margin-top-x-small">
       Setup:
-      <div className="flexbox margin-top">
-        <ol className="spaced-list margin-right-large" style={{ paddingInlineStart: 20 }}>
-          <li className="margin-top-none">
-            To use Two Factor Authentication, first download a third party authentication app such as{' '}
-            <a href="https://authy.com/download/" target="_blank" rel="noopener noreferrer">
-              Authy
-            </a>{' '}
-            or{' '}
-            <a href="https://support.google.com/accounts/answer/1066447" target="_blank" rel="noopener noreferrer">
-              Google Authenticator
-            </a>
-            .
+      <div className="flexbox">
+        <ol className={classes.orderedList}>
+          <li className="margin-top-none margin-bottom-small">
+            <Typography>Download a third party authentication app such as Authy or Google authenticator.</Typography>
           </li>
-          <li>Scan the QR code on the right with the authenticator app you just downloaded on your device.</li>
+          <li className="margin-bottom-small">
+            <Typography>On your authenticator application, tap the “+” icon and then select “Scan QR code” to scan the QR code below.</Typography>
+          </li>
+          {!qrImage ? <Loader show={!qrImage} /> : <img className={classes.qrImg} src={`data:image/png;base64,${qrImage}`} />}
           <li>
             <div>
-              Type in the generated code in the input field below and click Verify.
-              {validated2fa ? (
-                <div className="flexbox space-between centered margin-top margin-right margin-bottom" style={{ justifyContent: 'flex-end' }}>
-                  <CheckCircleIcon className="green" />
-                  <h3 className="green margin-left-small" style={{ textTransform: 'uppercase' }}>
-                    Verified
-                  </h3>
+              Once the code has been scanned, type in the generated code from the app to verify.
+              <Form
+                classes={classes}
+                showButtons={!validating2fa}
+                buttonColor="primary"
+                onSubmit={validate2faSetup}
+                handleCancel={onClose}
+                submitLabel="Verify and Save"
+              >
+                <TextInput className={classes.codeInput} hint="Verification code" label="Verification code" id="token2fa" validations="isLength:6,isNumeric" />
+              </Form>
+              {validating2fa && (
+                <div className="flexbox">
+                  <Loader show={true} />
+                  <Button variant="contained" color="primary" disabled={true}>
+                    Verifying...
+                  </Button>
                 </div>
-              ) : (
-                <>
-                  <Form className="margin-text-small" showButtons={!validating2fa} buttonColor="primary" onSubmit={validate2faSetup} submitLabel="Verify">
-                    <TextInput hint="Verification code" label="Verification code" id="token2fa" validations="isLength:6,isNumeric" required={true} />
-                  </Form>
-                  {validating2fa && (
-                    <div className="flexbox" style={{ alignItems: 'flex-end', justifyContent: 'flex-end', height: 'min-content' }}>
-                      <Loader show={true} />
-                      <Button variant="contained" color="primary" disabled={true} style={{ marginLeft: 30 }}>
-                        Verifying...
-                      </Button>
-                    </div>
-                  )}
-                </>
               )}
             </div>
           </li>
-          <li>Then each time you log in, you will be asked for a verification code which you can retrieve from the authentication app on your device.</li>
         </ol>
-        {!qrImage ? <Loader show={!qrImage} /> : <img src={`data:image/png;base64,${qrImage}`} style={{ maxHeight: '20vh' }} />}
-      </div>
-      <div className="flexbox" style={{ justifyContent: 'flex-end' }}>
-        <Button onClick={() => handle2FAState(twoFAStates.disabled)} style={{ marginRight: 10 }}>
-          Cancel
-        </Button>
-        <Button variant="contained" disabled={!validated2fa} onClick={onClose}>
-          Save
-        </Button>
       </div>
     </div>
   );
