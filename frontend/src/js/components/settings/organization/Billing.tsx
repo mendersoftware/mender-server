mendersoftware/mender-server

commit abfddc8480cd2e383f5db4c48eb95ce2240558ac

chore(gui): aligned billing notification in settings w/ updated design to ensure readability
Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

frontend/src/js/components/settings/organization/Billing.tsx
@@ -16,8 +16,8 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 // material ui
-import { Error as ErrorIcon } from '@mui/icons-material';
-import { Alert, Button, Typography } from '@mui/material';
+import { Error as ErrorIcon, OpenInNew } from '@mui/icons-material';
+import { Alert, Button, Typography, alpha } from '@mui/material';
 import { makeStyles } from 'tss-react/mui';
 
 import { SettingsItem } from '@northern.tech/common-ui/SettingsItem';
@@ -26,6 +26,7 @@ import { ADDONS, PLANS } from '@northern.tech/store/constants';
 import { getBillingProfile, getCard, getDeviceLimits, getIsEnterprise, getOrganization, getUserRoles } from '@northern.tech/store/selectors';
 import { useAppDispatch } from '@northern.tech/store/store';
 import { cancelRequest, getCurrentCard, getUserBilling } from '@northern.tech/store/thunks';
+import { isDarkMode } from '@northern.tech/store/utils';
 import { DeviceTierLimits } from '@northern.tech/types/MenderTypes';
 import { toggle } from '@northern.tech/utils/helpers';
 import dayjs from 'dayjs';
@@ -44,13 +45,11 @@ const useStyles = makeStyles()(theme => ({
       gridTemplateColumns: '1fr'
     }
   },
+  link: { ['&:hover']: { color: theme.palette.secondary.contrastText } },
   upgradeSection: {
-    backgroundColor: theme.palette.grey[100],
-    borderRadius: theme.spacing(0.5),
-    padding: theme.spacing(2),
-    paddingTop: 0
-  },
-  wrapper: { gap: theme.spacing(2) }
+    backgroundColor: isDarkMode(theme.palette.mode) ? alpha(theme.palette.info.light, theme.palette.action.selectedOpacity) : theme.palette.action.hover,
+    borderRadius: theme.spacing(0.5)
+  }
 }));
 
 const AddOnDescriptor = ({ addOns = [], isTrial }: { addOns: string[]; isTrial: boolean }) => {
@@ -172,7 +171,7 @@ export const CardDetails = props => {
 const UpgradeNote = ({ isTrial }) => {
   const { classes } = useStyles();
   return (
-    <div className={classes.upgradeSection}>
+    <div className={`padding-small padding-top-none margin-top margin-bottom ${classes.upgradeSection}`}>
       <SettingsItem
         classes={{ main: classes.fullWidthUpgrade }}
         title="Upgrade now"
@@ -182,11 +181,21 @@ const UpgradeNote = ({ isTrial }) => {
             : 'Upgrade to access more features, increase your device limit, and enhance your subscription with Add-ons.'
         }
       />
-      <div className={`flexbox align-items-center margin-top-x-small ${classes.wrapper}`}>
-        <Button component={Link} to="https://mender.io/pricing/plans" target="_blank" rel="noopener noreferrer" size="small">
+      <div className="flexbox align-items-center margin-top-x-small">
+        <Button
+          className="margin-right-small"
+          component={Link}
+          to="https://mender.io/pricing/plans"
+          target="_blank"
+          rel="noopener noreferrer"
+          size="small"
+          color="info"
+          variant="outlined"
+          endIcon={<OpenInNew />}
+        >
           Compare all plans
         </Button>
-        <Button color="primary" component={Link} to="/subscription" size="small" variant="contained">
+        <Button className={classes.link} color="secondary" component={Link} to="/subscription" size="small" variant="contained">
           Upgrade
         </Button>
       </div>
@@ -206,7 +215,6 @@ export const Billing = () => {
   const billing = useSelector(getBillingProfile);
   const { addons = [], plan: currentPlan = PLANS.os.id, trial: isTrial, trial_expiration } = organization;
   const dispatch = useAppDispatch();
-  const { classes } = useStyles();
 
   const planName = PLANS[currentPlan].name;
 
@@ -236,13 +244,13 @@ export const Billing = () => {
   return (
     <div style={{ maxWidth: 750 }}>
       <Typography variant="h6">Billing</Typography>
-      <div className={`flexbox column ${classes.wrapper}`}>
+      <div className="flexbox column">
         <SettingsItem
           title="Current plan"
           secondary={<PlanDescriptor plan={planName} isTrial={isTrial} trialExpiration={trial_expiration} deviceLimits={deviceLimits} />}
         />
         <SettingsItem title="Current Add-ons" secondary={<AddOnDescriptor addOns={enabledAddOns} isTrial={isTrial} />} />
-        {!isEnterprise && <UpgradeNote isTrial={isTrial} />}
+        {isEnterprise && <UpgradeNote isTrial={isTrial} />}
         <Typography className="margin-top-small" variant="subtitle1">
           Billing details
         </Typography>

commit 6639a6586acb82cd9c3897f7025d7daa76208b9b

fix(gui): restored billing notification visibility to only non-enterprise users
wrongly introduced in abfddc8480cd2e383f5db4c48eb95ce2240558ac

Changelog: None
Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

frontend/src/js/components/settings/organization/Billing.tsx
@@ -250,7 +250,7 @@ export const Billing = () => {
           secondary={<PlanDescriptor plan={planName} isTrial={isTrial} trialExpiration={trial_expiration} deviceLimits={deviceLimits} />}
         />
         <SettingsItem title="Current Add-ons" secondary={<AddOnDescriptor addOns={enabledAddOns} isTrial={isTrial} />} />
-        {isEnterprise && <UpgradeNote isTrial={isTrial} />}
+        {!isEnterprise && <UpgradeNote isTrial={isTrial} />}
         <Typography className="margin-top-small" variant="subtitle1">
           Billing details
         </Typography>

frontend/src/js/components/settings/organization/__snapshots__/Billing.test.tsx.snap
@@ -47,7 +47,272 @@ exports[`Billing Component > renders correctly 1`] = `
   letter-spacing: 0.01071em;
 }
 
-.emotion-14 {
+.emotion-13 {
+  background-color: rgba(0, 0, 0, 0.04);
+  border-radius: 4px;
+}
+
+.emotion-19.settings-item-main-content {
+  grid-template-columns: 1fr;
+}
+
+.emotion-21 {
+  display: -webkit-inline-box;
+  display: -webkit-inline-flex;
+  display: -ms-inline-flexbox;
+  display: inline-flex;
+  -webkit-align-items: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  position: relative;
+  box-sizing: border-box;
+  -webkit-tap-highlight-color: transparent;
+  background-color: transparent;
+  outline: 0;
+  border: 0;
+  margin: 0;
+  border-radius: 0;
+  padding: 0;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  vertical-align: middle;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  -webkit-text-decoration: none;
+  text-decoration: none;
+  color: inherit;
+  font-family: "Roboto","Helvetica","Arial",sans-serif;
+  font-weight: 500;
+  font-size: 0.875rem;
+  line-height: 1.75;
+  letter-spacing: 0.02857em;
+  text-transform: uppercase;
+  min-width: 64px;
+  padding: 6px 16px;
+  border: 0;
+  border-radius: 4px;
+  -webkit-transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  padding: 5px 15px;
+  border: 1px solid currentColor;
+  border-color: var(--variant-outlinedBorder, currentColor);
+  background-color: var(--variant-outlinedBg);
+  color: var(--variant-outlinedColor);
+  --variant-textColor: #0288d1;
+  --variant-outlinedColor: #0288d1;
+  --variant-outlinedBorder: rgba(2, 136, 209, 0.5);
+  --variant-containedColor: #fff;
+  --variant-containedBg: #0288d1;
+  padding: 3px 9px;
+  font-size: 0.8125rem;
+  -webkit-transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+}
+
+.emotion-21::-moz-focus-inner {
+  border-style: none;
+}
+
+.emotion-21.Mui-disabled {
+  pointer-events: none;
+  cursor: default;
+}
+
+@media print {
+  .emotion-21 {
+    -webkit-print-color-adjust: exact;
+    color-adjust: exact;
+  }
+}
+
+.emotion-21:hover {
+  -webkit-text-decoration: none;
+  text-decoration: none;
+}
+
+.emotion-21.Mui-disabled {
+  color: rgba(0, 0, 0, 0.26);
+}
+
+.emotion-21.Mui-disabled {
+  border: 1px solid rgba(0, 0, 0, 0.12);
+}
+
+@media (hover: hover) {
+  .emotion-21:hover {
+    --variant-containedBg: #01579b;
+    --variant-textBg: rgba(2, 136, 209, 0.04);
+    --variant-outlinedBorder: #0288d1;
+    --variant-outlinedBg: rgba(2, 136, 209, 0.04);
+  }
+}
+
+.emotion-21.MuiButton-loading {
+  color: transparent;
+}
+
+.emotion-22 {
+  display: inherit;
+  margin-right: -4px;
+  margin-left: 8px;
+  margin-right: -2px;
+}
+
+.emotion-22>*:nth-of-type(1) {
+  font-size: 18px;
+}
+
+.emotion-23 {
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  width: 1em;
+  height: 1em;
+  display: inline-block;
+  -webkit-flex-shrink: 0;
+  -ms-flex-negative: 0;
+  flex-shrink: 0;
+  -webkit-transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  fill: currentColor;
+  font-size: 1.5rem;
+}
+
+.emotion-24 {
+  display: -webkit-inline-box;
+  display: -webkit-inline-flex;
+  display: -ms-inline-flexbox;
+  display: inline-flex;
+  -webkit-align-items: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  position: relative;
+  box-sizing: border-box;
+  -webkit-tap-highlight-color: transparent;
+  background-color: transparent;
+  outline: 0;
+  border: 0;
+  margin: 0;
+  border-radius: 0;
+  padding: 0;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  vertical-align: middle;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  -webkit-text-decoration: none;
+  text-decoration: none;
+  color: inherit;
+  font-family: "Roboto","Helvetica","Arial",sans-serif;
+  font-weight: 500;
+  font-size: 0.875rem;
+  line-height: 1.75;
+  letter-spacing: 0.02857em;
+  text-transform: uppercase;
+  min-width: 64px;
+  padding: 6px 16px;
+  border: 0;
+  border-radius: 4px;
+  -webkit-transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  color: var(--variant-containedColor);
+  background-color: var(--variant-containedBg);
+  box-shadow: 0px 3px 1px -2px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 1px 5px 0px rgba(0,0,0,0.12);
+  --variant-textColor: #9c27b0;
+  --variant-outlinedColor: #9c27b0;
+  --variant-outlinedBorder: rgba(156, 39, 176, 0.5);
+  --variant-containedColor: #fff;
+  --variant-containedBg: #9c27b0;
+  padding: 4px 10px;
+  font-size: 0.8125rem;
+  -webkit-transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+}
+
+.emotion-24::-moz-focus-inner {
+  border-style: none;
+}
+
+.emotion-24.Mui-disabled {
+  pointer-events: none;
+  cursor: default;
+}
+
+@media print {
+  .emotion-24 {
+    -webkit-print-color-adjust: exact;
+    color-adjust: exact;
+  }
+}
+
+.emotion-24:hover {
+  -webkit-text-decoration: none;
+  text-decoration: none;
+}
+
+.emotion-24.Mui-disabled {
+  color: rgba(0, 0, 0, 0.26);
+}
+
+.emotion-24:hover {
+  box-shadow: 0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12);
+}
+
+@media (hover: none) {
+  .emotion-24:hover {
+    box-shadow: 0px 3px 1px -2px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 1px 5px 0px rgba(0,0,0,0.12);
+  }
+}
+
+.emotion-24:active {
+  box-shadow: 0px 5px 5px -3px rgba(0,0,0,0.2),0px 8px 10px 1px rgba(0,0,0,0.14),0px 3px 14px 2px rgba(0,0,0,0.12);
+}
+
+.emotion-24.Mui-focusVisible {
+  box-shadow: 0px 3px 5px -1px rgba(0,0,0,0.2),0px 6px 10px 0px rgba(0,0,0,0.14),0px 1px 18px 0px rgba(0,0,0,0.12);
+}
+
+.emotion-24.Mui-disabled {
+  color: rgba(0, 0, 0, 0.26);
+  box-shadow: none;
+  background-color: rgba(0, 0, 0, 0.12);
+}
+
+@media (hover: hover) {
+  .emotion-24:hover {
+    --variant-containedBg: #7b1fa2;
+    --variant-textBg: rgba(156, 39, 176, 0.04);
+    --variant-outlinedBorder: #9c27b0;
+    --variant-outlinedBg: rgba(156, 39, 176, 0.04);
+  }
+}
+
+.emotion-24.MuiButton-loading {
+  color: transparent;
+}
+
+.emotion-24:hover {
+  color: #fff;
+}
+
+.emotion-26 {
   background-color: #fff;
   color: rgba(0, 0, 0, 0.87);
   -webkit-transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
@@ -70,11 +335,11 @@ exports[`Billing Component > renders correctly 1`] = `
   background-color: rgb(255, 244, 229);
 }
 
-.emotion-14 .MuiAlert-icon {
+.emotion-26 .MuiAlert-icon {
   color: #ed6c02;
 }
 
-.emotion-15 {
+.emotion-27 {
   margin-right: 12px;
   padding: 7px 0;
   display: -webkit-box;
@@ -85,7 +350,7 @@ exports[`Billing Component > renders correctly 1`] = `
   opacity: 0.9;
 }
 
-.emotion-16 {
+.emotion-28 {
   -webkit-user-select: none;
   -moz-user-select: none;
   -ms-user-select: none;
@@ -102,13 +367,13 @@ exports[`Billing Component > renders correctly 1`] = `
   font-size: inherit;
 }
 
-.emotion-17 {
+.emotion-29 {
   padding: 8px 0;
   min-width: 0;
   overflow: auto;
 }
 
-.emotion-18 {
+.emotion-30 {
   margin: 0;
   font-family: "Roboto","Helvetica","Arial",sans-serif;
   font-weight: 500;
@@ -118,7 +383,7 @@ exports[`Billing Component > renders correctly 1`] = `
   color: #d32f2f;
 }
 
-.emotion-20 {
+.emotion-32 {
   display: -webkit-inline-box;
   display: -webkit-inline-flex;
   display: -ms-inline-flexbox;
@@ -177,37 +442,37 @@ exports[`Billing Component > renders correctly 1`] = `
   transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
 }
 
-.emotion-20::-moz-focus-inner {
+.emotion-32::-moz-focus-inner {
   border-style: none;
 }
 
-.emotion-20.Mui-disabled {
+.emotion-32.Mui-disabled {
   pointer-events: none;
   cursor: default;
 }
 
 @media print {
-  .emotion-20 {
+  .emotion-32 {
     -webkit-print-color-adjust: exact;
     color-adjust: exact;
   }
 }
 
-.emotion-20:hover {
+.emotion-32:hover {
   -webkit-text-decoration: none;
   text-decoration: none;
 }
 
-.emotion-20.Mui-disabled {
+.emotion-32.Mui-disabled {
   color: rgba(0, 0, 0, 0.26);
 }
 
-.emotion-20.Mui-disabled {
+.emotion-32.Mui-disabled {
   border: 1px solid rgba(0, 0, 0, 0.12);
 }
 
 @media (hover: hover) {
-  .emotion-20:hover {
+  .emotion-32:hover {
     --variant-containedBg: #c62828;
     --variant-textBg: rgba(211, 47, 47, 0.04);
     --variant-outlinedBorder: #d32f2f;
@@ -215,7 +480,7 @@ exports[`Billing Component > renders correctly 1`] = `
   }
 }
 
-.emotion-20.MuiButton-loading {
+.emotion-32.MuiButton-loading {
   color: transparent;
 }
 
@@ -293,22 +558,84 @@ exports[`Billing Component > renders correctly 1`] = `
             </div>
           </div>
         </div>
+        <div
+          class="padding-small padding-top-none margin-top margin-bottom emotion-13"
+        >
+          <div
+            class="flexbox column settings-item-base emotion-1 margin-top-small "
+          >
+            <div
+              class="flexbox column settings-item-content emotion-1 emotion-3 "
+            >
+              <h6
+                class="MuiTypography-root MuiTypography-subtitle1 emotion-4"
+              >
+                Upgrade now
+              </h6>
+              <div
+                class="settings-item-main-content emotion-5 emotion-19"
+              >
+                <div
+                  class="MuiTypography-root MuiTypography-body2 emotion-6"
+                >
+                  Upgrade to access more features, increase your device limit, and enhance your subscription with Add-ons.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="flexbox align-items-center margin-top-x-small"
+          >
+            <a
+              class="MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButton-outlinedInfo MuiButton-sizeSmall MuiButton-outlinedSizeSmall MuiButton-colorInfo MuiButton-root MuiButton-outlined MuiButton-outlinedInfo MuiButton-sizeSmall MuiButton-outlinedSizeSmall MuiButton-colorInfo margin-right-small emotion-21"
+              href="https://mender.io/pricing/plans"
+              rel="noopener noreferrer"
+              tabindex="0"
+              target="_blank"
+            >
+              Compare all plans
+              <span
+                class="MuiButton-icon MuiButton-endIcon MuiButton-iconSizeSmall emotion-22"
+              >
+                <svg
+                  aria-hidden="true"
+                  class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium emotion-23"
+                  data-testid="OpenInNewIcon"
+                  focusable="false"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3z"
+                  />
+                </svg>
+              </span>
+            </a>
+            <a
+              class="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedSecondary MuiButton-sizeSmall MuiButton-containedSizeSmall MuiButton-colorSecondary MuiButton-root MuiButton-contained MuiButton-containedSecondary MuiButton-sizeSmall MuiButton-containedSizeSmall MuiButton-colorSecondary emotion-24"
+              data-discover="true"
+              href="/subscription"
+              tabindex="0"
+            >
+              Upgrade
+            </a>
+          </div>
+        </div>
         <h6
           class="MuiTypography-root MuiTypography-subtitle1 margin-top-small emotion-4"
         >
           Billing details
         </h6>
         <div
-          class="MuiPaper-root MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation0 MuiAlert-root MuiAlert-colorWarning MuiAlert-standardWarning MuiAlert-standard emotion-14"
+          class="MuiPaper-root MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation0 MuiAlert-root MuiAlert-colorWarning MuiAlert-standardWarning MuiAlert-standard emotion-26"
           role="alert"
           style="--Paper-shadow: none;"
         >
           <div
-            class="MuiAlert-icon emotion-15"
+            class="MuiAlert-icon emotion-27"
           >
             <svg
               aria-hidden="true"
-              class="MuiSvgIcon-root MuiSvgIcon-fontSizeInherit emotion-16"
+              class="MuiSvgIcon-root MuiSvgIcon-fontSizeInherit emotion-28"
               data-testid="ReportProblemOutlinedIcon"
               focusable="false"
               viewBox="0 0 24 24"
@@ -319,7 +646,7 @@ exports[`Billing Component > renders correctly 1`] = `
             </svg>
           </div>
           <div
-            class="MuiAlert-message emotion-17"
+            class="MuiAlert-message emotion-29"
           >
             Your account is not set up for automatic billing. If you believe this is a mistake, please contact 
             <a
@@ -338,7 +665,7 @@ exports[`Billing Component > renders correctly 1`] = `
         style="gap: 8px;"
       >
         <h6
-          class="MuiTypography-root MuiTypography-h6 emotion-18"
+          class="MuiTypography-root MuiTypography-h6 emotion-30"
         >
           Delete account
         </h6>
@@ -349,7 +676,7 @@ exports[`Billing Component > renders correctly 1`] = `
         </p>
         <div>
           <button
-            class="MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButton-outlinedError MuiButton-sizeMedium MuiButton-outlinedSizeMedium MuiButton-colorError MuiButton-root MuiButton-outlined MuiButton-outlinedError MuiButton-sizeMedium MuiButton-outlinedSizeMedium MuiButton-colorError emotion-20"
+            class="MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButton-outlinedError MuiButton-sizeMedium MuiButton-outlinedSizeMedium MuiButton-colorError MuiButton-root MuiButton-outlined MuiButton-outlinedError MuiButton-sizeMedium MuiButton-outlinedSizeMedium MuiButton-colorError emotion-32"
             tabindex="0"
             type="button"
           >