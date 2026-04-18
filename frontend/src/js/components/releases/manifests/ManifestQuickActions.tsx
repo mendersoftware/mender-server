4290ef49d4124b3387d43e1c846bf82231c94747
Manuel Zedel <manuel.zedel@northern.tech>
2026-04-18 23:27:32 +0200

    feat(gui): added manifest quick actions component
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

---
+++ frontend/src/js/components/releases/manifests/ManifestQuickActions.tsx
@@ -0,0 +1,186 @@
+// Copyright 2026 Northern.tech AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+import { type ReactNode, useCallback, useMemo, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import {
+  FileCopyOutlined as FileCopyOutlinedIcon,
+  HighlightOffOutlined as HighlightOffOutlinedIcon,
+  LabelOutlined as LabelOutlinedIcon,
+  Replay as ReplayIcon
+} from '@mui/icons-material';
+import { ClickAwayListener, SpeedDial, SpeedDialAction, SpeedDialIcon, Typography, alpha, getOverlayAlpha } from '@mui/material';
+import { speedDialActionClasses } from '@mui/material/SpeedDialAction';
+import { makeStyles } from 'tss-react/mui';
+
+import storeActions from '@northern.tech/store/actions';
+import { DEPLOYMENT_ROUTES } from '@northern.tech/store/constants';
+import { getManifestsListState, getSelectedManifests, getUserCapabilities } from '@northern.tech/store/selectors';
+import { isDarkMode } from '@northern.tech/store/utils';
+import { toggle } from '@northern.tech/utils/helpers';
+import pluralize from 'pluralize';
+
+const { setSnackbar } = storeActions;
+
+const useStyles = makeStyles()(theme => ({
+  container: {
+    display: 'flex',
+    position: 'fixed',
+    bottom: theme.spacing(6.5),
+    right: theme.spacing(6.5),
+    zIndex: 10,
+    minWidth: 'max-content',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    pointerEvents: 'none',
+    [`& .${speedDialActionClasses.staticTooltipLabel}`]: {
+      minWidth: 'max-content'
+    }
+  },
+  fab: { marginBottom: theme.spacing(2), marginRight: theme.spacing(2) },
+  label: {
+    background: isDarkMode(theme.palette.mode) ? alpha('#fff', getOverlayAlpha(6)) : theme.palette.common.white,
+    boxShadow: isDarkMode(theme.palette.mode) ? 'none' : theme.shadows[6],
+    padding: `${theme.spacing(1)} ${theme.spacing(2)}`,
+    borderRadius: theme.spacing(0.5),
+    marginRight: theme.spacing(1),
+    marginBottom: theme.spacing(3)
+  }
+}));
+
+interface ActionCallbacks {
+  onCopyManifest: (selection: number[]) => void;
+  onCreateDeployment: (selection: number[]) => void;
+  onDeleteManifest: (selection: number[]) => void;
+  onTagManifest: (selection: number[]) => void;
+}
+
+interface ManifestAction {
+  action: (context: ActionCallbacks & { selection: number[] }) => void;
+  icon: ReactNode;
+  isApplicable: ({ selectedRows, userCapabilities }: { selectedRows: number[]; userCapabilities: Record<string, boolean | string[]> }) => boolean;
+  key: string;
+  title: (pluralized: string) => string;
+}
+
+const defaultActions: ManifestAction[] = [
+  {
+    action: ({ onCreateDeployment, selection }) => onCreateDeployment(selection),
+    icon: <ReplayIcon />,
+    isApplicable: ({ userCapabilities: { canDeploy }, selectedRows }) => canDeploy && selectedRows.length === 1,
+    key: 'deploy',
+    title: () => 'Create a deployment for this Manifest'
+  },
+  {
+    action: ({ onCopyManifest, selection }) => onCopyManifest(selection),
+    icon: <FileCopyOutlinedIcon />,
+    isApplicable: ({ selectedRows }) => selectedRows.length === 1,
+    key: 'copy',
+    title: () => 'Create a copy from this Manifest'
+  },
+  {
+    action: ({ onTagManifest, selection }) => onTagManifest(selection),
+    icon: <LabelOutlinedIcon />,
+    isApplicable: ({ userCapabilities: { canManageReleases } }) => canManageReleases,
+    key: 'tag',
+    title: (pluralized: string) => `Tag ${pluralized}`
+  },
+  {
+    action: ({ onDeleteManifest, selection }) => onDeleteManifest(selection),
+    icon: <HighlightOffOutlinedIcon className="red" />,
+    isApplicable: ({ userCapabilities: { canManageReleases } }) => canManageReleases,
+    key: 'delete',
+    title: (pluralized: string) => `Delete ${pluralized}`
+  }
+];
+
+export const ManifestQuickActions = () => {
+  const [showActions, setShowActions] = useState(false);
+  const { classes } = useStyles();
+  const { selection: selectedRows } = useSelector(getManifestsListState);
+  const selectedManifests = useSelector(getSelectedManifests);
+  const userCapabilities = useSelector(getUserCapabilities);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const actions = useMemo(
+    () =>
+      defaultActions.reduce<ManifestAction[]>((accu, action) => {
+        if (action.isApplicable({ userCapabilities, selectedRows })) {
+          accu.push(action);
+        }
+        return accu;
+      }, []),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [JSON.stringify(userCapabilities), selectedRows]
+  );
+
+  const onCreateDeployment = useCallback(
+    (selection: number[]) => {
+      if (selection.length !== 1) {
+        return;
+      }
+      const { name: manifestName } = selectedManifests[0];
+      navigate(`${DEPLOYMENT_ROUTES.active.route}?open=true&release=${encodeURIComponent(manifestName)}`, { state: { internal: true } });
+    },
+    [navigate, selectedManifests]
+  );
+
+  const onCopyManifest = useCallback(() => {
+    dispatch(setSnackbar('Creating a copy from a Manifest is not yet supported'));
+  }, [dispatch]);
+
+  const onTagManifest = useCallback(() => {
+    dispatch(setSnackbar('Tagging Manifests is not yet supported'));
+  }, [dispatch]);
+
+  const onDeleteManifest = useCallback(() => {
+    dispatch(setSnackbar('Deleting Manifests is not yet supported'));
+  }, [dispatch]);
+
+  const actionCallbacks: ActionCallbacks = { onCreateDeployment, onCopyManifest, onTagManifest, onDeleteManifest };
+
+  const handleShowActions = () => setShowActions(toggle);
+
+  const handleClickAway = () => setShowActions(false);
+
+  const pluralized = pluralize('Manifest', selectedRows.length);
+
+  if (!actions.length) {
+    return null;
+  }
+  return (
+    <div className={classes.container}>
+      <Typography variant="body1" className={classes.label}>
+        {`${selectedRows.length} ${pluralized} selected`}
+      </Typography>
+      <ClickAwayListener onClickAway={handleClickAway}>
+        <SpeedDial className={classes.fab} ariaLabel="manifest-actions" icon={<SpeedDialIcon />} onClick={handleShowActions} open={Boolean(showActions)}>
+          {actions.map(action => (
+            <SpeedDialAction
+              key={action.key}
+              aria-label={action.key}
+              icon={action.icon}
+              slotProps={{ tooltip: { title: action.title(pluralized), open: true } }}
+              onClick={() => action.action({ ...actionCallbacks, selection: selectedRows })}
+            />
+          ))}
+        </SpeedDial>
+      </ClickAwayListener>
+    </div>
+  );
+};
+
+export default ManifestQuickActions;
