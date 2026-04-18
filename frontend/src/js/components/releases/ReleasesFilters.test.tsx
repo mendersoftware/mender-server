901846957b1b1c9b59d59d57ea96ec18d11fb781
Manuel Zedel <manuel.zedel@northern.tech>
2026-04-18 23:27:31 +0200

    refactor(gui): extracted release filters into dedicated component
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

---
+++ frontend/src/js/components/releases/ReleasesFilters.test.tsx
@@ -0,0 +1,31 @@
+// Copyright 2026 Northern.tech AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+import { render } from '@/testUtils';
+import { undefineds } from '@northern.tech/testing/mockData';
+
+import ReleasesFilters from './ReleasesFilters';
+
+const classes = {
+  container: 'test-container',
+  searchNote: 'test-search-note'
+};
+
+describe('ReleasesFilters Component', () => {
+  it('renders correctly', async () => {
+    const { baseElement } = render(<ReleasesFilters classes={classes} />);
+    const view = baseElement.firstChild.firstChild;
+    expect(view).toMatchSnapshot();
+    expect(view).toEqual(expect.not.stringMatching(undefineds));
+  });
+});
