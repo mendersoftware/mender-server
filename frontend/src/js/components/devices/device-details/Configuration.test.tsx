5f4c7551eeccacbf3247e691853d7e0733c4506d
Peter Grzybowski <peter@northern.tech>
2026-04-26 10:14:06 +0200

    rvr
    Changelog: Title
    Ticket: MEN-6415
    Signed-off-by: Peter Grzybowski <peter@northern.tech>

--- frontend/src/js/components/devices/device-details/Configuration.test.tsx
+++ frontend/src/js/components/devices/device-details/Configuration.test.tsx
@@ -19,8 +19,6 @@ import userEvent from '@testing-library/user-event';
 import { vi } from 'vitest';
 
 import Configuration, { ConfigEditingActions, ConfigEmptyNote, ConfigUpToDateNote, ConfigUpdateFailureActions, ConfigUpdateNote } from './Configuration';
-import {waitMs} from "@reduxjs/toolkit/src/tests/utils/helpers";
-import {timeouts} from "retry";
 
 describe('tiny components', () => {
   [ConfigEditingActions, ConfigUpdateFailureActions, ConfigUpdateNote, ConfigEmptyNote, ConfigUpToDateNote].forEach(async Component => {
