e5b39ed5530ce516100652ed81ca293658c2c171
Manuel Zedel <manuel.zedel@northern.tech>
2026-09-17 12:53:33 +0200

    test(e2e): ensure enterprise tenants have usable tests for current scenarios
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- frontend/tests/e2e_tests/runner/test-execution.js
+++ frontend/tests/e2e_tests/runner/test-execution.js
@@ -29,7 +29,7 @@ const createTenant = async (credentials, config, addons = [], options = '') => {
   try {
     const tenantResult = await composeExec(
       'tenantadm',
-      `tenantadm create-org --name=${name} --username=${username} --password=${password} --device-limit=-1 ${addons.map(addon => `--addon=${addon}`).join(' ')} ${options}`,
+      `tenantadm create-org --name=${name} --username=${username} --password=${password} --device-limit=-1 --micro-device-limit=100 --system-device-limit=200 ${addons.map(addon => `--addon=${addon}`).join(' ')} ${options}`,
       config
     );
     return tenantResult.out.trim();
@@ -63,7 +63,7 @@ const createServiceProviderTenant = async (credentials, config) =>
         { ...credentials, name: tenantNames.serviceProvider, username: credentials.spTenant },
         config,
         [],
-        '--device-limit 100 --micro-device-limit=200'
+        '--device-limit=300'
       );
       // updateOne with $set is already idempotent
       await composeExec('mongo', 'mongosh --eval "db.getSiblingDB("tenantadm").tenants.updateOne({},{$set:{max_child_tenants:100}})"', config);
